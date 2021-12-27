a3aeae3e8bece24a17f3a0bf156b32d9eed0a161
notbasem <bhamid@live.at>
2021-12-27 14:41:11 +0100

    Hotfix gitignore

--- src/main/java/com/company/Server/models/MonsterCard.java
+++
@@ -1,8 +0,0 @@
-package com.company.Server.models;
-
-public class MonsterCard extends Card {
-
-    public MonsterCard(String id, String name, float damage) {
-        super(id, name, damage);
-    }
-}
--- src/main/java/com/company/Server/models/SpellCard.java
+++
@@ -1,8 +0,0 @@
-package com.company.Server.models;
-
-public class SpellCard extends Card {
-
-    public SpellCard(String id, String name, float damage) {
-        super(id, name, damage);
-    }
-}
